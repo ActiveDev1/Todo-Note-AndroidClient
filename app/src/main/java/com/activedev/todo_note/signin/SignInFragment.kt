cfa14c2bc9397f9a3b0c06215c55ab217c458efb
ActiveDev1 <sarbazmahdi.2014@gmail.com>
2020-07-15 22:15:05 +0430

    Update .gitignore

--- app/src/main/java/com/activedev/todo_note/signin/SignInFragment.kt
+++ app/src/main/java/com/activedev/todo_note/signin/SignInFragment.kt
@@ -1,10 +1,11 @@
-package com.activedev.todo_note
+package com.activedev.todo_note.signin
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.activedev.todo_note.R
 
 class SignInFragment : Fragment() {
 
